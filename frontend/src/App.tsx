df9f31895f8a571f32a8c9b028b03785dbd14dec
Samuuil <atanas872@gmail.com>
2024-03-14 14:21:55 +0200

    Login and SignUp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
+import Login from './components/Login'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import SignUp from './components/SignUp'
+
+const router = createBrowserRouter([ 
+	{
+		path: '/',
+		element: <Login/>,
+		errorElement: <div>Brat 404 situaciq</div>,
+	},
+	{
+		path: '/Sign-Up',
+		element: <SignUp/>,
+		errorElement: <div>Brat 404 situaciq</div>,
+	},
+]);
 
 function App() {
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+//   return (<>
+// 			<RouterProvider router={router} />
+//   			</>);
 }
 
 export default App
