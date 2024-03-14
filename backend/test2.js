4720f50f48a643bd306b6c7997f21c145e2aafcd
Konstantin Zlatev <kzlatev07@gmail.com>
2024-03-14 15:35:53 +0200

    Add test file for reminders

--- backend/test2.js
+++ backend/test2.js
@@ -1,7 +1,7 @@
-const schedule = require("node-schedule");
+const schedule = require("node-schedule")
 
-const date = new Date('2024-03-14T12:34:00.000+5:30')
+const date = new Date('2024-03-14T13:10:00.000+5:30')
 
-schedule.scheduleJob(date, () =>{
-    console.log('Job ran at', new Date().toString())
+schedule.scheduleJob('* * * * *', () =>{
+    console.log('Water your plant');
 })
