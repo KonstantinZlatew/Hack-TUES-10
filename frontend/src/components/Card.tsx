753f74a32b6681ab73e65a2a10bf602cbc2cb462
Gabo1234567890 <gabonenov@gmail.com>
2024-03-14 19:32:07 +0200

    card fix

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -6,7 +6,7 @@ interface PlantCardProps {
 function Card({title, imageUrl}: PlantCardProps){
   return (
       <div
-        className="ml-12 w-72 h-96 mt-12 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        className="ml-12 w-72 h-96 mt-12 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-white">
         <a href="#!">
           <img
             className="rounded-t-lg"
@@ -15,7 +15,7 @@ function Card({title, imageUrl}: PlantCardProps){
         </a>
         <div className="p-6">
           <h5
-            className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            className="mb-2 text-xl font-medium leading-tight text-zinc-950">
             {title}</h5>
             <button
               type="button"
