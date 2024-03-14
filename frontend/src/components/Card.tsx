f720a679640933d77778dffc3dde5a3b0d55fa9b
Tan4y <100035220+Tan4y@users.noreply.github.com>
2024-03-14 19:43:54 +0200

    remove unnecessary

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -6,7 +6,7 @@ interface PlantCardProps {
 function Card({title, imageUrl}: PlantCardProps){
   return (
       <div
-        className="ml-12 w-72 h-96 mt-12 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-white">
+        className="ml-12 w-72 h-96 mt-12 block rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-white">
         <a href="#!">
           <img
             className="rounded-t-lg"
@@ -19,13 +19,13 @@ function Card({title, imageUrl}: PlantCardProps){
             {title}</h5>
             <button
               type="button"
-              className="inline-block bg-emerald-900 rounded-full  mr-6 bg-primary my-2 px-4 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white
+              className="inline-block bg-emerald-800 rounded-full  mr-6 bg-primary my-2 px-4 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white
               transition duration-150 ">
               Learn More
             </button>
             <button
               type="button"
-              className="inline-block bg-emerald-900 rounded-full bg-primary my-2 px-4 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white
+              className="inline-block bg-emerald-800 rounded-full bg-primary my-2 px-4 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white
               transition duration-150 ">
               Favorites
             </button>
