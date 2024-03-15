ce2cf9acb3c75a7cd66261406dab7c4a5cc1aae9
amazingdanny <blaskoff_2003@abv.bg>
2024-03-15 19:43:20 +0200

    trying to fix login AGAIN

--- backend/app.js
+++ backend/app.js
@@ -123,7 +123,7 @@ app.get("/get_plant_by_id/:id", async (req, res) => {
 
 app.get("/Login/:data", async (req, res) => {
     try {
-        const {email, password} = req.params;
+        const {email, password} = req.body;
         const db = new dbService();
         user = await db.Login(email, password);
         //console.log(result);
