3e23ee0f884ced1ea77c137741bbd9c9f5de8a83
Gabo1234567890 <gabonenov@gmail.com>
2024-03-16 02:46:29 +0200

    learn more problem

--- frontend/src/components/PlantPage.tsx
+++ frontend/src/components/PlantPage.tsx
@@ -36,10 +36,10 @@ function PlantPage () {
     }, [plantId]);
     return (
         <>
-            <div className="h-full bg-gradient-to-r from-emerald-800 to-pink-300">
+            <div className="h-full bg-gradient-to-r from-emerald-800 to-pink-300 flex-1">
             {plant ? (
                 <div className="flex">
-                    <img className="rounded-lg  h-[700px] w-[600px] m-3" src={plant.image_url} alt="Plant" />
+                    <img className="rounded-lg  h-[500px] w-[400px] m-3" src={plant.image_url} alt="Plant" />
                     <div className="flex flex-col items-center flex flex-1 py-36 justify-center h-full">
                         <h1 className="text-3xl px-72 text-emerald-900 text-center font-bold mb-4 pt-28">Information about the plant</h1>
                         <p className="text-lg"><span className="font-bold text-emerald-900">NAME: </span> {plant.name}</p>
